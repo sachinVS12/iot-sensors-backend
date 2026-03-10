695951532ccf9c5766f44379a067dea8cbef87a5
Sachin V S <vs1sachi12@gmail.com>
2026-03-10 17:07:40 +0530

    controllers4

--- routers/admin-router.js
+++ routers/admin-router.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { login, adminLogin } = require("../controllers/admin-controller.js");
+const { createCompany } = require("../controllers/auth-controller.js");
 const router = express.Router();
 
 router.route("/login").post(login);
 router.route("/admin/login").post(adminLogin);
+router.route("./createcompany/").post(createCompany);
+router.route("./manager/login").post(managerAslogin);
 
 module.exports = router;
