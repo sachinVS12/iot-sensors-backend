eb31f5970ff5813bc281e8b43b7b1ad6a565364e
Sachin V S <vs1sachi12@gmail.com>
2026-03-26 15:20:11 +0530

    controllers51

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -16,37 +16,3 @@ router.route("/company/:companyId").get(getSingleCompany);
 router.route("/companies/:id").delete(deleteCompany);
 router.route("/createSupervisor").post(createSupervisor);
 module.exports = router;
-
-// server.js
-const winston = require("winston");
-const connectDB = require("./env/db");
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const errorHandler = require("./middleware/error");
-const cookieParser = require("cookie-parser");
-const fielupload = require("express-fileupload");
-const dotenv = require("dotenv");
-const authRoute = require("./Routers/authRoute");
-const mqttRoute = require("./Routers/mqttRouters");
-const supportemailRoute = require("./Routers/supportemailRouter");
-const backupdbRoute = require("./Routers/backupdbRoute");
-
-// load environment variable
-dotenv.config({ path: "./.env" });
-
-// express intialize
-const app = express();
-
-// logger configuration
-const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(),
-    winston.format.json(),
-  ),
-  transports: [
-    new winston.transports.File({ filename: error.log, level: "error" }),
-    new winston.transports.File({ filename: "combine.log" }),
-  ],
-});
--- routers/mqttRouter.js
+++ routers/mqttRouter.js
@@ -2151,4 +2151,33 @@ const sendMailtoCustomer = async (req, res, next) => {
   }
 };
 
+// temp work
+
+const { parentPort } = require("worker_threads");
+const {
+  getLatestLiveMessage,
+} = require("C:\Users\sujan\OneDrive\Desktop\sarayu-web\Project\sarayu-node-backend\middlewares\mqttHandler.js");
+
+// Process topic streams
+parentPort.on("message", async ({ action, topics }) => {
+  if (action === "processBatch") {
+    const results = [];
+
+    for (const topic of topics) {
+      try {
+        const latestMessage = await getLatestLiveMessage(topic);
+        if (latestMessage) {
+          results.push({ topic, message: latestMessage });
+        }
+      } catch (error) {
+        parentPort.postMessage({
+          error: `Error processing topic ${topic}: ${error.message}`,
+        });
+      }
+    }
+
+    parentPort.postMessage({ results });
+  }
+});
+
 module.exports = router;
