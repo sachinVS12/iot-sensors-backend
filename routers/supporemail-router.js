aef1478b5fffa859449f01c2fc1cbbe5e0c998ba
Sachin V S <vs1sachi12@gmail.com>
2026-03-27 16:02:26 +0530

    controllers54

--- routers/supporemail-router.js
+++ routers/supporemail-router.js
@@ -15,3 +15,9 @@ const {
   createMailCredAndSetActive,
 } = require("../controllers/supportmail-controller");
 const router = express.Router();
+
+// Attach the Socket.IO instance to the request object
+router.use((req, res, next) => {
+  req.io = req.app.get("socketio");
+  next();
+});
